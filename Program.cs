55b2632f7299f18cc525db0fa15920424b97b398
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-13 12:06:16 +0530

    added: Home page welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the solutions page for Day-5 Functional Programming!");
+        }
+    }
+}
