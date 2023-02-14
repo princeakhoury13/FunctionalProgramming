8a4ed2a7d92ef332fbc62aa0737951c1be3a7a64
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-14 19:30:52 +0530

    added: find factors of a number

---
+++ Factors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class Factors
+    {
+        public static void FindFactors()
+        {
+            Console.WriteLine("Enter a number to find its prime factors:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Prime factors of " + n + ": ");
+
+            
+            for (int i = 2; i * i <= n; i++)
+            {
+                
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+                }
+            }
+
+            if (n > 1)
+            {
+                Console.Write(n);
+            }
+
+            
+        }
+    }
+}
