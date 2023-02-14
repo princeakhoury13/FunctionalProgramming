22a3f55442f3c023cc3b76853af8d81520c48aba
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-14 19:35:57 +0530

    added: Find quotient and remainder of given numbers

---
+++ QnR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class QnR
+    {
+        public static void FindQnR()
+        {
+            Console.WriteLine("Enter the divident");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : "+remainder);
+        }
+    }
+}
